a7f132fc624f07f8e296fd6cd17aae04f581b896
sunblack <A3056573732@outlook.com>
2025-07-22 16:17:10 +0800

    员工分页查询业务代码开发

---
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -0,0 +1,31 @@
+package com.sky.service;
+
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeeDTO;
+import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
+import com.sky.entity.Employee;
+import com.sky.result.PageResult;
+
+public interface EmployeeService {
+
+    /**
+     * 员工登录
+     * @param employeeLoginDTO
+     * @return
+     */
+    Employee login(EmployeeLoginDTO employeeLoginDTO);
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
+    void save(EmployeeDTO employeeDTO);
+
+    /**
+     * 分页查询
+     * @param employeePageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+}
